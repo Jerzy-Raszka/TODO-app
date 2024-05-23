a36ef417435787c8de5a6341f27162a74e9d1356
Jerzy laptop <jerzyraszka2000@gmail.com>
2024-05-23 12:46:53 +0200

    Added folders to one project

---
+++ todoBack/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+const todoSchema = new mongoose.Schema({
+    name: String,
+    status: Boolean,
+});
+
+const todosItem = mongoose.model('todosItem', todoSchema);
+
+
+mongoose.connect('mongodb+srv://Jirru:eQNToZhbNvEtI1cB@cluster0.hibixil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+
+app.get('/todo', async (req, res) => {
+    res.send(await todosItem.find({}))
+});
+
+app.post('/todo', async (req, res) => {
+    const newTodoItem = await todosItem.create({ name: req.body.name, status: req.body.status });
+    res.send(newTodoItem)
+});
+
+app.put('/todo', async (req, res) => {
+    await todosItem.findByIdAndUpdate({_id: req.body._id},{name: req.body.name, status: req.body.status})
+    res.send(await todosItem.find({}))
+})
+
+app.delete('/todo', async (req, res) => {
+    await todosItem.findByIdAndDelete({_id: req.body._id})
+    res.send(await todosItem.find({}))
+})
+
+app.listen(port)
