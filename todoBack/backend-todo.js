a8ad43b3ceced1bc241f4cfa07702385eedb43ed
Jirru <jerzyraszk2000@gmail.com>
2024-05-25 00:19:47 +0200

    fix: Access Control Allow Origin port number

--- todoBack/backend-todo.js
+++ todoBack/backend-todo.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     express.json()
