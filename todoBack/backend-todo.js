8b04bbf136bc9c419c00ac683e6fd840e84234b1
Jirru <jerzyraszk2000@gmail.com>
2024-05-25 12:42:28 +0200

    feat: added updateItem function(not working)

--- todoBack/backend-todo.js
+++ todoBack/backend-todo.js
@@ -3,12 +3,13 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 
+app.use(express.json())
+
 app.use(function (req, res, next) {
 
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    express.json()
     next();
 })
 
--- todoFront/script-todo.js
+++ todoFront/script-todo.js
@@ -1,4 +1,3 @@
-
     fetch('http://localhost:3000/todo')
     .then(response => {
         if (!response.ok) {
@@ -9,12 +8,21 @@
         .then(data=>{
             console.log(data);
             data.forEach(todosItem => {
-                const markup = `<div><span>${todosItem.name}</span> <input type="checkbox" value="${todosItem.status}" onchange="updateItem('${todosItem._id}')"/></div>`;
+                const markup = `<div><span>${todosItem.name}</span> <input type="checkbox" value='${todosItem.status}' onchange="updateItem('${todosItem._id}', '${todosItem.status}', '${todosItem.name}')"/></div>`;
                 document.getElementById('todoItems').insertAdjacentHTML('beforeend', markup);
             });
         })
         .catch(err => console.log(err));
 
-const updateItem = (id) => {
-    //TODO update by id here
+const updateItem = (id, status, name) => {
+    const formData = new FormData();
+    formData.append('_id', id);
+    formData.append('name', name);
+    formData.append('status', status);
+
+    return fetch('http://localhost:3000/todo', {
+        method: 'PUT',
+        body: formData
+    }).then(response => response.json())
+        .then(response => console.log(response))
 }
