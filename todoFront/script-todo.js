b452a8bd0688069ae3befef9e5810e4ee70d933f
chatis <chatisek2420@gmail.com>
2024-05-28 22:19:43 +0200

    chore: code refactor

--- todoFront/script-todo.js
+++ todoFront/script-todo.js
@@ -14,37 +14,26 @@ fetch('http://localhost:3000/todo')
         globalData.forEach(todosItem => {
             const markup = `<div id='${todosItem._id + 'div'}'><input type="text" id='${todosItem._id + 'name'}' value='${todosItem.name}' onblur="updateName('${todosItem._id}')"/><input type="checkbox" id='${todosItem._id}' onchange="updateStatus('${todosItem._id}')"/><button onclick="deleteItem('${todosItem._id}')">Delete</button></div>`;
             document.getElementById('todoItems').insertAdjacentHTML('beforeend', markup);
-            if (todosItem.status === true) {
-                document.getElementById(todosItem._id).checked = true;
-            }
+            document.getElementById(todosItem._id).checked = todosItem.status;
         });
     })
     .catch(err => console.log(err));
 
 const updateStatus = (_id) => {
-    const index = globalData.findIndex(x => x._id === _id);
-    globalData[index].status = !globalData[index].status
-    if (globalData[index].status === true) {
-        document.getElementById(_id).checked = true;
-    }
+    const item = globalData.find(x => x._id === _id);
+    item.status = !item.status
+    document.getElementById(_id).checked = item.status;
     putData(_id)
 }
 
 const updateName = (_id) => {
-    const index = globalData.findIndex(x => x._id === _id);
-    globalData[index].name = document.getElementById(_id + 'name').value;
+    globalData.find(x => x._id === _id).name = document.getElementById(_id + 'name').value;
     putData(_id)
 }
 
 const putData = (_id) => {
-    const index = globalData.findIndex(x => x._id === _id);
-    return fetch('http://localhost:3000/todo', {
-        method: 'PUT',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(globalData[index])
-    }).then(response => response.json())
+    const item = globalData.find(x => x._id === _id);
+    resolveRequest('PUT', item)
         .then(response => console.log(response))
 }
 
@@ -52,26 +41,15 @@ const deleteItem = (_id) => {
     const index = globalData.findIndex(x => x._id === _id);
     globalData.splice(index, 1);
     document.getElementById(_id + 'div').remove();
-    return fetch('http://localhost:3000/todo', {
-        method: 'DELETE',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({_id: _id})
-    }).then(response => response.json())
+    resolveRequest('DELETE',{_id: _id})
         .then(response => console.log(response))
 }
 
 const newItem = () => {
-    console.log(document.getElementById("newToDoItem").value)
-    const newItem = {name: document.getElementById("newToDoItem").value, status: false};
-    return fetch('http://localhost:3000/todo', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newItem)
-    }).then(response => response.json())
+    const newItemName = document.getElementById("newToDoItem").value;
+    console.log(newItemName)
+    const newItem = {name: newItemName, status: false};
+    resolveRequest('POST',newItem)
         .then(data => {
             globalData.push(data)
             console.log(globalData)
@@ -79,4 +57,19 @@ const newItem = () => {
             document.getElementById('todoItems').insertAdjacentHTML('beforeend', markup);
         })
 
+}
+
+const resolveRequest = (method, body) => {
+    return fetch('http://localhost:3000/todo', requestFactory(method, body))
+        .then(response => response.json())
+}
+
+const requestFactory = (method, body) => {
+    return {
+        method: method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
+    }
 }
